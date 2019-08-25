7c6299d14ed8dfdde02bbf6631d989a3fd6f2fe6
AniHakobyan <annie2016l@gmail.com>
2019-08-25 23:42:34 +0400

    small fix

--- Python_Starter/functions.py
+++ Python_Starter/functions.py
@@ -1,8 +1,30 @@
+# # define a function which prints all the odd numbers between the given range
+# def print_odd_numbers(from_numb, to_numb):
+#     for i in range(from_numb, to_numb):
+#         if i % 2 != 0:
+#             print(i)
 
-def print_odd_numbers(from_numb, to_numb):
-    for i in range(from_numb, to_numb):
-        if i % 2 != 0:
-            print(i)
-        i += 1
+# print_odd_numbers(3, 15)
 
-print_odd_numbers(3, 15)
+
+# # define a function which calculates factorial
+# def numb_factorial(number):
+#     factorial = 1
+#     while(number > 0):
+#         factorial = factorial * number
+#         number -= 1
+#     print(factorial)
+ 
+# numb_factorial(5)    # 120
+# numb_factorial(1)    # 1
+# numb_factorial(3)    # 6
+
+
+#
+x = 7
+print(x) #
+print(id(x)) #
+
+x += 1
+print(x)
+print(id(x))
