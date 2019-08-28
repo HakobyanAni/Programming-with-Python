72226114f7780787c248d3fdc2993ddc9adafb7f
AniHakobyan <annie2016l@gmail.com>
2019-08-28 22:37:23 +0400

    functions_ 2 added

---
+++ functions_2.py
@@ -0,0 +1,31 @@
+def square_of_two_numbers(a, b):
+    result = pow(a, 2) + 2 * a * b + pow(b, 2)
+    return result
+
+res = square_of_two_numbers(2, 3)
+print(res)
+
+
+# call of function
+def personal_info(first_name = 'Helen', last_name='Pitt', age=15):
+    print('first name: ', first_name, sep='\t')
+    print('last name: ', last_name, sep='\t')
+    print('age: ', age, sep='\t')
+
+# 1st version
+print(personal_info('John', 'Lenon', 25))
+
+# 2nd version
+print(personal_info(
+     first_name='Teo',
+     last_name='Brown',
+      age=30,))
+
+# 3rd version
+print(personal_info(first_name='Joseph', age=35, last_name='Morgan'))
+
+# 4th version
+print(personal_info())
+
+# 5th version
+print(personal_info(last_name='Kayer', first_name='Nahi'))
