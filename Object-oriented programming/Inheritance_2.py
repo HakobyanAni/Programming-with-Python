ca0895a00cebaabb3205759d7d2a8769bd93ffe7
HakobyanAni <ani.hakobian.8@gmail.com>
2019-09-21 22:51:19 +0400

    added mro

--- Object-oriented programming/Inheritance_2.py
+++ Object-oriented programming/Inheritance_2.py
@@ -5,13 +5,24 @@ class BaseClass(object):
         print("Method's class", __class__.__name__)
         print("Instance's class", type(self).__name__)
 
-class ChildClass(BaseClass):
+class ChildClass_1(BaseClass):
     pass
 
+class ChildClass_2(ChildClass_1):
+    def descript_class(self):
+        print("Method's class", __class__.__name__)
+
+class  ChildClass_3(ChildClass_2):  
+    pass   
 
 if __name__ == "__main__":
     base_instance = BaseClass()          # Method's class BaseClass
     base_instance.descript_class()       # Instance's class BaseClass
 
-    child_instance = ChildClass()        # Method's class BaseClass
-    child_instance.descript_class()      # Instance's class ChildClass
+    child_instance = ChildClass_1()      # Method's class BaseClass
+    child_instance.descript_class()      # Instance's class ChildClass_1
+
+    child_instance_3 = ChildClass_3()
+    print(ChildClass_3.__mro__)          # __mro__ shows Method resolution order
+                                         # (<class '__main__.ChildClass_3'>, <class '__main__.ChildClass_2'>, 
+                                         # <class '__main__.ChildClass_1'>, <class '__main__.BaseClass'>, <class 'object'>)
